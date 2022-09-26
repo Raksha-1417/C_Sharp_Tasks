cce265e296099e36781888c636d17ddfe9457ac9
Raksha-1417 <raksha.rk14@gmail.com>
2022-09-26 23:24:19 +0530

    Add project files.

---
+++ Control.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Tasks
+{
+    internal class Control
+    {
+        public static void Main()
+        {
+            evenno();
+            Console.WriteLine("Th grades are");
+            Control g= new Control();
+            g.grade();
+             /* int num = 5;
+               if (num == 5)
+               {
+                   Console.WriteLine("Number is 5");
+
+               }
+               else if (num < 6)
+               {
+                   Console.WriteLine("Number is less than 6");
+
+               }
+               else
+               {
+                   Console.WriteLine("Invalid");
+               }*/
+
+
+           }
+            public static void evenno()
+
+            {
+                Console.WriteLine("Enter a Number");
+                int n = Convert.ToInt32(Console.ReadLine());
+                if (n % 2 == 0)
+                {
+                    Console.WriteLine("Even Number");
+
+                }
+                else
+                {
+                    Console.WriteLine("Odd Number");
+                }
+                Console.ReadLine();
+
+            }
+        public void grade()
+        {
+            Console.WriteLine("Enter the number to check Grade");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n < 0 || n > 100)
+            {
+                Console.WriteLine("wrong number");
+            }
+            else if (n >= 0 && n < 47)
+            {
+                Console.WriteLine("Fail");
+            }
+            else if (n >= 47 && n < 60)
+            {
+                Console.WriteLine("D Grade");
+            }
+            else if (n >= 60 && n < 70)
+            {
+                Console.WriteLine("C Grade");
+            }
+            else if (n >= 70 && n < 80)
+            {
+                Console.WriteLine("B Grade");
+            }
+            else if (n >= 80 && n < 90)
+            {
+                Console.WriteLine("A Grade");
+            }
+            else if (n >= 90 && n <= 100)
+            {
+                Console.WriteLine("A+ Grade");
+            }
+            Console.ReadLine();
+
+        }
+        }
+    }
+
---
+++ enummeration.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Tasks
+{
+    public enum Days
+    {
+        Monday=1, Tuesday=2,Wednesday=3,Thursday=4,Friday=5,Saturday=6
+    }
+
+
+    internal class enummeration
+    {
+        public static void Main()
+        {
+            foreach(int i in Enum.GetValues(typeof(Days)))
+                Console.WriteLine("Week days are "+i);
+            foreach(string str in Enum.GetNames(typeof(Days)))
+                Console.WriteLine(str);
+            Console.ReadLine();
+
+        }
+    }
+}
